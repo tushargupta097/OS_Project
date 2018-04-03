fd2bd960ef5f43b396639142b6c32ec40b37b2f2
tushargupta097 <37018603+tushargupta097@users.noreply.github.com>
2018-04-03 09:53:38 +0000

    Update Assignment.c

--- Assignment.c
+++ Assignment.c
@@ -2,10 +2,29 @@
 #include<stdlib.h>
 #include<pthread.h>
 
+int MIN_PID=300;
+int MAX_PID=5000;
+pthread_mutex_t mutex;
+int tid = 300;
+struct pid_tab
+{
+    int pid;
+    bool bitmap;
+}pidArr[4700];
+int allocate_map(void)                                
+{
+    int i,j;
+    for(i = MIN_PID, j =0; i <= MAX_PID; i++, j++)
+    {
+        pidArr[j].bitmap = 0;
+    }
+    if(i == MAX_PID && j == 4700)
+    return 1;
+}
+
+
 void * threadcall(void * P){
 	
-	while(thread<100)
-	{
         pthread_mutex_lock(&mutex);     
         if (thread >= 100)
         {
@@ -15,12 +34,28 @@ void * threadcall(void * P){
 		thread++;
 	pthread_mutex_unlock(&mutex);
 }
+
 	
+void release_pid(int pid)                              
+{
+	int i=0;
+    for( i = 0; i <= 4700; i++)
+    {
+        if(pidArr[i].pid == pid)
+        {
+        	printf("\npid released: %d",pidArr[i].pid);
+            pidArr[i].bitmap = 0;
+            
+        } 
+    }
+    
+}
+
 }
 int main()
 {
     pthread_t thread[50];
-    
+    allocate_map();
     for(i = 0; i < 100; i++)
     {
         pthread_mutex_init(&mutex, NULL);
